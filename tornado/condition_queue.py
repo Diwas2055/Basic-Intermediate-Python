a5a51a3405d5847f3d8226ab819693e2e366f35e
Diwash Bhandari <diwasb54@gmail.com>
2022-05-16 15:01:26 +0545

    Learn Coroutlines

---
+++ tornado/condition_queue.py
@@ -0,0 +1,29 @@
+# Example 2
+
+# Condition
+# A condition allows one or more coroutines to wait until notified.
+from tornado import gen
+from tornado.ioloop import IOLoop
+from tornado.locks import Condition
+
+my_condition = Condition()
+
+@gen.coroutine  # implements generator-based coroutines.
+def waiter():
+    print("I'll wait right here")
+    yield my_condition.wait()
+    print("Received notification now doing my things")
+
+@gen.coroutine
+def notifier():
+    yield gen.sleep(10)
+    print("About to notify")
+    my_condition.notify()
+    print("Done notifying")
+
+@gen.coroutine
+def runner():
+    # Wait for waiter() and notifier() in parallel
+    yield([waiter(), notifier()])
+
+results = IOLoop.current().run_sync(runner)
---
+++ tornado/simple_queue.py
@@ -0,0 +1,29 @@
+# Example 1
+from tornado import gen, queues
+from tornado.ioloop import IOLoop
+
+@gen.coroutine  # implements generator-based coroutines.
+def consumer(queue, num_expected):
+    for _ in range(num_expected):
+        # heavy I/O or network task
+        print('got: %s' % (yield queue.get()))
+
+
+@gen.coroutine
+def producer(queue, num_items):
+    for i in range(num_items):
+        print('putting %s' % i)
+        yield queue.put(i)
+
+@gen.coroutine
+def main():
+    """
+    Starts producer and consumer and wait till they finish
+    """
+    yield [producer(q, producer_num_items), consumer(q, producer_num_items)]
+
+queue_size = 1
+producer_num_items = 5
+q = queues.Queue(queue_size)
+
+results = IOLoop.current().run_sync(main)
