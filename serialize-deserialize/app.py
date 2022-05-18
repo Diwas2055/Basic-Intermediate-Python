8db7c30b98225041fd4eeefadd302ba08b1126b4
Diwash Bhandari <diwasb54@gmail.com>
2022-05-18 17:39:22 +0545

    Serialize and Deserialze Concept

--- serialize-deserialize/app.py
+++ serialize-deserialize/app.py
@@ -1,13 +1,13 @@
 """ Serialization is a process in which an object is 
  transformed into a format that can be stored/save (in a file or memory buffer) """
 
-# There are two basic formats for JSON data.
-# Either in a string or the object datastructures.
-# The object datastructures, in Python, consists of lists and dictionaries nested inside each other.
-# The object datastructures allows one to use python methods (for lists and dictionaries) to add,
-# list, search and remove elements from the datastructures.
-# The String format is mainly used to pass the data into another program
-# or load into a datastructures.
+# todo:- There are two basic formats for JSON data.
+# todo:- Either in a string or the object datastructures.
+# todo:- The object datastructures, in Python, consists of lists and dictionaries nested inside each other.
+# todo:- The object datastructures allows one to use python methods (for lists and dictionaries) to add,
+# todo:- list, search and remove elements from the datastructures.
+# todo:- The String format is mainly used to pass the data into another program
+# todo:- or load into a datastructures.
 
 import pickle
 import json
@@ -15,8 +15,8 @@ from pprint import pprint
 
 """ Json.loads """
 
-# To load JSON back to a data structure, use the "loads" method.
-# This method takes a string and turns it back into the json object datastructures:
+# !To load JSON back to a data structure, use the "loads" method.
+# !This method takes a string and turns it back into the json object datastructures:
 
 json_string = """{
     "Name": "Jennifer Smith",
@@ -28,7 +28,7 @@ print(json.loads(json_string))
 
 
 """ Json.dumps """
-# json.dumps() function converts a Python object into a json string.
+# !json.dumps() function converts a Python object into a json string.
 
 json_string = json.dumps([1, 2, 3, "a", "b", "c"])
 print(json_string)
@@ -36,24 +36,24 @@ print(json_string)
 
 """ Pickle """
 
-# The pickle module is used for implementing binary protocols for
-# serializing and de-serializing a Python object structure.
+# ?The pickle module is used for implementing binary protocols for
+# ?serializing and de-serializing a Python object structure.
 
 """ Serialize """
-# dumps() – This function is called to serialize an object hierarchy.
+# todo:- dumps() – This function is called to serialize an object hierarchy.
 
 friend1 = {"Dan": [20, "London", 3234], "Maria": [22, "Paris", 7876]}
 friend2 = {"Joey": [23, "New-york", 32394], "Ross": [25, "Washington", 786776]}
 friends = (friend1, friend2)
-    #wb:- write in binary mode
+# !wb:- write in binary mode
 with open('friends.txt', 'wb') as f:
     # pickle.dump(data,file)
     pickle.dump(friends, f)
 
 
 """ Deserialize """
-# loads() – This function is called to de-serialize a data stream.
-    #rb:- read in binary mode
+# todo:- loads() – This function is called to de-serialize a data stream.
+# !rb:- read in binary mode
 with open('friends.txt', 'rb') as f:
     obj = pickle.load(f)
     print(type(obj))
