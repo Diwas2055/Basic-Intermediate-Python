880286230b6d009aeada2ff04abca04e3de1ca60
Diwash Bhandari <diwasb54@gmail.com>
2022-05-26 11:45:48 +0545

    Type of Queue

--- async.py
+++ async.py
@@ -1,11 +1,16 @@
+# Importing the asyncio module.
 import asyncio
 
+# Creating an event loop.
 e_loop = asyncio.get_event_loop()
 
 
 async def hello():
     print('Searching ...')
     print('CPU is free & not blocking IO....')
+    
+    # A coroutine that suspends the execution of the current task 
+    # and allows other tasks to run.
     await asyncio.sleep(3)
     print('Waiting for task (1) t0 finish ...')
     await asyncio.sleep(2)
@@ -16,4 +21,5 @@ async def hello():
 
 # hello()
 
+# Running the event loop until the future is done.
 e_loop.run_until_complete(hello())
