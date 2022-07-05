64583465886c0057242a903abb4693dcd11fe437
Diwash Bhandari <diwasb54@gmail.com>
2022-07-05 17:39:19 +0545

    Web Scarpping Demo

--- web-scrapping/web-scrapping.py
+++ web-scrapping/web-scrapping.py
@@ -1,10 +1,11 @@
+# List Article  from DEV API 
 from pprint import pprint
 import sys
 import requests
 import json
 
-# List Article  from DEV API 
 limit=input("Enter the limit: ")
+
 list_articles=requests.get(f"https://dev.to/api/articles?per_page={limit}")
 list_articles=list_articles.json()
 
@@ -12,6 +13,7 @@ print("Number of Articles",len(list_articles))
 
 # for list_article in list_articles:
 
+# The above code is creating a json file with the list of articles.
 with open('web-scrapping/article.json', 'w') as json_file:
   json.dump(list_articles, json_file)
   json_file.close()
