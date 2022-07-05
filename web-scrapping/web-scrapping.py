d1f85610c6ab68af30df077941ed7c170a6863c6
Diwash Bhandari <diwasb54@gmail.com>
2022-07-05 17:45:46 +0545

    Web Scarpiing with request handling

--- web-scrapping/web-scrapping.py
+++ web-scrapping/web-scrapping.py
@@ -4,17 +4,27 @@ import sys
 import requests
 import json
 
-limit=input("Enter the limit: ")
+limit=input("Enter the limit [ 1 - 1000 ]: ")
 
-list_articles=requests.get(f"https://dev.to/api/articles?per_page={limit}")
-list_articles=list_articles.json()
+try:
+    list_articles=requests.get(f"https://dev.to/api/articles?per_page={limit}")
+    list_articles=list_articles.json()
 
-print("Number of Articles",len(list_articles))
+    print("Number of Articles",len(list_articles))
 
-# for list_article in list_articles:
+    # for list_article in list_articles:
 
-# The above code is creating a json file with the list of articles.
-with open('web-scrapping/article.json', 'w') as json_file:
-  json.dump(list_articles, json_file)
-  json_file.close()
-  print("File created in Json format")
+    # The above code is creating a json file with the list of articles.
+    with open('web-scrapping/article.json', 'w') as json_file:
+      json.dump(list_articles, json_file)
+      json_file.close()
+      print("File created in Json format")
+except requests.exceptions.Timeout:
+    # Maybe set up for a retry, or continue in a retry loop
+    print("Timeout")
+except requests.exceptions.TooManyRedirects:
+    # Tell the user their URL was bad and try a different one
+    print("TooManyRedirects")
+except requests.exceptions.RequestException as e:
+    # catastrophic error. bail.
+    raise SystemExit(e)
